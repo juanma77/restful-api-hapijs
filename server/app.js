79dae19dac312bd28b00119a125ade0d04d6fa67
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-05-05 16:09:50 -0500

    initial server configuration added

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const Hapi = require('hapi');
+
+// Creamos una nueva instancia del servidor hapi 
+const server = Hapi.server({
+    port: 3100, // Obligatorio 
+    host: 'localhost', // Obligatorio 
+    app: {} // Opcional
+});
+
+const initServer = async () => {
+    try{
+        await server.start();
+        console.log(`Server running on port: ${server.info.uri}`);
+        
+    }catch(error){
+        console.log('Error at starting the server')
+    }
+};
+
+initServer();
